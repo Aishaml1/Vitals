40999f1b8cc49ad76a979108fdc57262bab73546
Aishaml1 <Aisha.lugg1@icloud.com>
2021-11-26 16:31:00 -0500

    login and signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -10,3 +12,16 @@ class Home(LoginView):
 def about(request):
     return render(request, 'about.html')
 
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('patients_index')
+        else:
+            error_message = 'Invalid sign up - try again'
+            form = UserCreationForm()
+            context = {'form': form, 'error_message': error_message}
+            return render(request, 'signup.html', context)
